4815b7fcad3660b2f2e72ea70d8556c40bdc7034
Basant-innovation <59922781+Basant-innovation@users.noreply.github.com>
2020-09-11 12:46:01 +0200

    :building_construction: Folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home/home.component';
+import { FooterComponent } from './shared/layout/footer/footer/footer.component';
+import { NavbarComponent } from './shared/layout/navbar/navbar/navbar.component';
+import { HeaderComponent } from './shared/layout/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
